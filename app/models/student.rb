7d3e0ad093fcc7d64a78f0470a4080f59ea393cf
Thuy Nguyen <thuy.nguyen@techpropulsionlabs.com>
2012-10-10 15:42:40 +0700

    Update validate and UI for add student

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,6 +14,8 @@ class Student < ActiveRecord::Base
 
   # VALIDATION
   validates_presence_of :first_name, :last_name
+  validates :first_name, :uniqueness => { :scope => :teacher_id,
+    :message => "should not be duplicated name" }
 end
 
 # == Schema Information
