b3de2f7a17c8591beb52e3cb6564db431111f5c5
Man Vuong <man.vuong@techpropulsionlabs.com>
2012-10-12 15:11:19 +0700

    Update sample data

--- db/sample_data.rb
+++ db/sample_data.rb
@@ -1,17 +1,6 @@
 # This file should contain dummy records creation needed to demonstrate the app or to test.
 # The data can then be loaded with the rake db:sample_data.
 
-admin = User.find_by_id(1)
-if admin
-  20.times do
-    admin.students.create!({
-      :first_name => Faker::Name.first_name,
-      :last_name => Faker::Name.last_name,
-      :birthday => Time.now - 16.years
-    })
-  end
-end
-
 5.times do 
   begin
     teacher = User.create!({
