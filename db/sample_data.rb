90f3eb33438ba349a17c914076f33a63b1d5924d
Thuy Nguyen <thuy.nguyen@techpropulsionlabs.com>
2012-10-17 16:41:23 +0700

    update sample data

--- db/sample_data.rb
+++ db/sample_data.rb
@@ -3,31 +3,31 @@
 
 admin = User.first
 
-#5.times do 
-#  begin
-#    teacher = User.new({
-#      :email => Faker::Internet.email,
-#      :first_name => Faker::Name.first_name,
-#      :last_name => Faker::Name.last_name,
-#      :password => "123456",
-#      :password_confirmation => "123456",
-#    })
-#    teacher.parent = admin
-#    teacher.skip_confirmation!
-#    teacher.save!
-#    puts "[Sampler] Created teacher #{teacher.email} / 123456"
+5.times do 
+  begin
+    teacher = User.new({
+      :email => Faker::Internet.email,
+      :first_name => Faker::Name.first_name,
+      :last_name => Faker::Name.last_name,
+      :password => "123456",
+      :password_confirmation => "123456",
+    })
+    teacher.parent = admin
+    teacher.skip_confirmation!
+    teacher.save!
+    puts "[Sampler] Created teacher #{teacher.email} / 123456"
 
-#    20.times do
-#      teacher.students.create!({
-#        :first_name => Faker::Name.first_name,
-#        :last_name => Faker::Name.last_name,
-#        :birthday => Time.now - 16.years
-#      })
-#    end
-#  rescue Exception => e
-#    puts "[Sampler] Error in creating sample teacher: #{e.inspect}"
-#  end
-#end
+    20.times do
+      teacher.students.create!({
+        :first_name => Faker::Name.first_name,
+        :last_name => Faker::Name.last_name,
+        :birthday => Time.now - 16.years
+      })
+    end
+  rescue Exception => e
+    puts "[Sampler] Error in creating sample teacher: #{e.inspect}"
+  end
+end
 Role.create(:name => "Teacher")
 Role.create(:name => "Admin")
 Role.create(:name => "Parent")
