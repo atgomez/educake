3830e9f17091b0da8804fe4375a4cacf8f0fd290
Man Vuong <man.vuong@techpropulsionlabs.com>
2012-10-22 16:52:28 +0700

    Fix bug in user registration

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -5,43 +5,49 @@ class UserRegistrationsController < Devise::RegistrationsController
   def new
     if params[:user_token].blank?
       redirect_to "/"
-    elsif @sharing = StudentSharing.find_by_confirm_token(params[:user_token]) && @sharing && !@sharing.confirmed?
-      resource = build_resource({})
-      respond_with resource
     else
-      redirect_to "/"
-    end    
+      @sharing = StudentSharing.find_by_confirm_token(params[:user_token])
+      if @sharing && !@sharing.confirmed?
+        resource = build_resource({})
+        respond_with resource
+      else
+        redirect_to "/"
+      end
+    end
   end
 
   # POST /resource
   def create
     if params[:user_token].blank?
       redirect_to "/"
-    elsif @sharing = StudentSharing.find_by_confirm_token(params[:user_token]) && @sharing && !@sharing.confirmed?
-      build_resource      
-      # To prevent hacking
-      self.resource.email      = @sharing.email
-      self.resource.first_name = @sharing.first_name
-      self.resource.last_name  = @sharing.last_name
-      self.resource.role_id    = @sharing.role_id
+    else
+      @sharing = StudentSharing.find_by_confirm_token(params[:user_token])
+      if @sharing && !@sharing.confirmed?
+        build_resource      
+        # To prevent hacking
+        self.resource.email      = @sharing.email
+        self.resource.first_name = @sharing.first_name
+        self.resource.last_name  = @sharing.last_name
+        self.resource.role_id    = @sharing.role_id
 
-      if resource.save
-        if resource.active_for_authentication?
-          set_flash_message :notice, :signed_up if is_navigational_format?
-          # sign_in(resource, :bypass => true)
-          sign_in(resource_name, resource)
-          respond_with resource, :location => after_sign_up_path_for(resource)
+        if resource.save
+          if resource.active_for_authentication?
+            set_flash_message :notice, :signed_up if is_navigational_format?
+            # sign_in(resource, :bypass => true)
+            sign_in(resource_name, resource)
+            respond_with resource, :location => after_sign_up_path_for(resource)
+          else
+            set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+            expire_session_data_after_sign_in!
+            respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+          end
         else
-          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-          expire_session_data_after_sign_in!
-          respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+          clean_up_passwords resource
+          respond_with resource
         end
       else
-        clean_up_passwords resource
-        respond_with resource
+        redirect_to "/"
       end
-    else
-      redirect_to "/"
     end
   end
 end
