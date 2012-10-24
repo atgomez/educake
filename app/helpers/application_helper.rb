07a43b124bee558892d5e15efaa7c5c1f43a3491
Man Vuong <man.vuong@techpropulsionlabs.com>
2012-10-24 12:06:33 +0700

    Finish banner.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  # Get the current tab
+  def current_tab
+    @current_tab ||= "home"
+  end
+
+  def set_current_tab(item)
+    (current_tab.to_s == item.to_s) ? "active" : ""
+  end
+
   # Render pagination section.
   # The data_source must be paginated by Will_paginate or Kaminari
   def render_pagination(data_source, paging_params = {}, options = {})
