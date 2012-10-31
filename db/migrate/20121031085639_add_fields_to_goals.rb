bf69beace84185ce4bd95a3724b6b2843d2c0662
An Nguyen <an.nguyen@techpropulsionlabs.com>
2012-11-01 01:55:38 +0700

    temporary remove check null for migration existing database

--- db/migrate/20121031085639_add_fields_to_goals.rb
+++ db/migrate/20121031085639_add_fields_to_goals.rb
@@ -1,7 +1,7 @@
 class AddFieldsToGoals < ActiveRecord::Migration
   def change
     add_column :goals, :baseline, :integer, :default => 0
-    add_column :goals, :baseline_date, :date, :null => false
+    add_column :goals, :baseline_date, :date
     add_column :goals, :trial_days_total, :integer, :default => 0
     add_column :goals, :trial_days_actual, :integer, :default => 0
     add_column :goals, :is_archived, :bool, :default => false
