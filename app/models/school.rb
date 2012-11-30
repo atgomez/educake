fa0058358d78b04f2365d3c13a60561714b0a661
An Nguyen <nguyenan169@gmail.com>
2012-11-30 16:01:08 +0700

    add error to school edit

--- app/models/school.rb
+++ app/models/school.rb
@@ -35,7 +35,7 @@ class School < ActiveRecord::Base
   validates_uniqueness_of :name, :scope => [:state]
   validates_presence_of :address1, :name, :state, :phone
   validates_presence_of :city, :message => "City can't be blank."
-  validates :zipcode, :length => { :maximum => 5, :too_long => "%{count} characters is the maximum allowed" }
+  validates :zipcode, :length => { :maximum => 5, :too_long => "Zip code with %{count} characters is the maximum allowed" }
   validates :name, :length => { :maximum => 255, :too_long => "%{count} characters is the maximum allowed" }
  
   # CLASS METHODS
