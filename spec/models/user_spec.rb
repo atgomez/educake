7411f7da0db06978a7359b00bb61edf62ae2793a
Thuy Nguyen <thuy.nguyen@techpropulsionlabs.com>
2012-12-10 09:56:10 +0700

    rspec for  user (cont)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,79 +37,79 @@
 require 'spec_helper'
 
 describe User do
-#  describe "attributes" do
-#    it { should have_attribute(:email) }
-#    #it { should have_attribute(:password) }
-#    #it { should have_attribute(:password_confirmation) }
-#    #it { should have_attribute(:remember_me) }
-#    it { should have_attribute(:first_name) }
-#    it { should have_attribute(:last_name) }
-#    it { should have_attribute(:classroom) }
-#    it { should have_attribute(:confirmed_at) }
-#    it { should have_attribute(:parent_id) }
-#    it { should have_attribute(:role_id) }
-#    it { should have_attribute(:school_id) }
-#  end
+  describe "attributes" do
+    it { should have_attribute(:email) }
+    #it { should have_attribute(:password) }
+    #it { should have_attribute(:password_confirmation) }
+    #it { should have_attribute(:remember_me) }
+    it { should have_attribute(:first_name) }
+    it { should have_attribute(:last_name) }
+    it { should have_attribute(:classroom) }
+    it { should have_attribute(:confirmed_at) }
+    it { should have_attribute(:parent_id) }
+    it { should have_attribute(:role_id) }
+    it { should have_attribute(:school_id) }
+  end
 
-#  describe 'associations' do
-#    it { should have_many(:children).class_name("User")}#.foreign_key('patient_id')
-#    it { should have_one(:student_sharing).dependent(:destroy)}
-#    it { should have_many(:student_sharings).dependent(:destroy)}
-#    it { should have_many(:shared_students).through(:student_sharings)}#.source(:student)
-#    it { should have_many(:students).dependent(:destroy)}#.foreign_key("teacher_id")
-#    it { should belong_to(:role) }
-#    it { should belong_to(:school) }
-#  end
-#  
-#  describe 'validates' do
-#    it { should validate_presence_of(:first_name)}
-#    it { should validate_presence_of(:last_name)}
-#    it { should validate_presence_of(:email)}
-#    it { should validate_presence_of(:role)}#, :unless => :is_super_admin?)}
-#    it { should validate_presence_of(:school_id)}#, :if => :is_not_admin?)}
-#    it {should ensure_length_of(:first_name).is_at_most(15)}
-#    it {should ensure_length_of(:last_name).is_at_most(15)}
-#  end
+  describe 'associations' do
+    it { should have_many(:children).class_name("User")}#.foreign_key('patient_id')
+    it { should have_one(:student_sharing).dependent(:destroy)}
+    it { should have_many(:student_sharings).dependent(:destroy)}
+    it { should have_many(:shared_students).through(:student_sharings)}#.source(:student)
+    it { should have_many(:students).dependent(:destroy)}#.foreign_key("teacher_id")
+    it { should belong_to(:role) }
+    it { should belong_to(:school) }
+  end
   
-#  describe "With role" do
-#    let(:admin_role) { FactoryGirl.create(:role, :name => "Admin1")}
-#    let(:admin) {FactoryGirl.create(:admin, :role => Role[:admin1])}
-#    it "when input is symbol and retrurn list user with admin role" do
-#      admin_role
-#      admin
-#      rs = User.with_role :admin1 
-#      rs.count.should eq(1)
-#    end 
-#    it "when input is string and retrurn list user with admin role" do
-#      admin_role
-#      admin
-#      rs = User.with_role "admin1" 
-#      rs.count.should eq(1)
-#    end
-#    it "when input is integer and retrurn empty" do
-#      rs = User.with_role 54354
-#      rs.should be_empty  
-#    end
-#    it "retrurn empty list user with teacher role" do
-#      rs = User.with_role :teacher
-#      rs.should be_empty
-#    end
-#  end 
-#  
-#  describe "Super admins" do 
-#    let(:super_admin) { FactoryGirl.create(:super_admin)}
-#    it "return super admins" do
-#      User.destroy_all
-#      super_admin 
-#      rs = User.super_admins
-#      rs.count.should eq(1)
-#    end 
-#    it "return empty list super admins" do
-#      User.destroy_all
-#      rs = User.super_admins
-#      rs.should be_empty
-#    end 
-#  end 
+  describe 'validates' do
+    it { should validate_presence_of(:first_name)}
+    it { should validate_presence_of(:last_name)}
+    it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:role)}#, :unless => :is_super_admin?)}
+    it { should validate_presence_of(:school_id)}#, :if => :is_not_admin?)}
+    it {should ensure_length_of(:first_name).is_at_most(15)}
+    it {should ensure_length_of(:last_name).is_at_most(15)}
+  end
+  
+  describe "With role" do
+    let(:admin_role) { FactoryGirl.create(:role, :name => "Admin1")}
+    let(:admin) {FactoryGirl.create(:admin, :role => Role[:admin1])}
+    it "when input is symbol and retrurn list user with admin role" do
+      admin_role
+      admin
+      rs = User.with_role :admin1 
+      rs.count.should eq(1)
+    end 
+    it "when input is string and retrurn list user with admin role" do
+      admin_role
+      admin
+      rs = User.with_role "admin1" 
+      rs.count.should eq(1)
+    end
+    it "when input is integer and retrurn empty" do
+      rs = User.with_role 54354
+      rs.should be_empty  
+    end
+    it "retrurn empty list user with teacher role" do
+      rs = User.with_role :teacher
+      rs.should be_empty
+    end
+  end 
+  
+  describe "Super admins" do 
+    let(:super_admin) { FactoryGirl.create(:super_admin)}
+    it "return super admins" do
+      User.destroy_all
+      super_admin 
+      rs = User.super_admins
+      rs.count.should eq(1)
+    end 
+    it "return empty list super admins" do
+      User.destroy_all
+      rs = User.super_admins
+      rs.should be_empty
+    end 
+  end 
   describe "Admins" do 
     let(:admin) { FactoryGirl.create(:admin)}
     it "return list admins" do
@@ -155,5 +155,7 @@ describe User do
       rs = User.unblocked
       rs.should be_empty
     end 
-  end 
+  end
+  
+  
 end
