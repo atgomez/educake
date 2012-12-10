0f0fe9ff1afa6a920ddd67eb5456c8aab36fd2ac
Man Vuong <man.vuong@techpropulsionlabs.com>
2012-12-10 18:56:25 +0700

    Improve invitation form

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,8 +2,7 @@ class InvitationsController < ApplicationController
   cross_role_action :new, :create, :edit, :update, :destroy
 
   def new 
-    @user = StudentSharing.new 
-    
+    @user = StudentSharing.new    
   end 
   
   def create
@@ -24,6 +23,13 @@ class InvitationsController < ApplicationController
   
   def update
     @user = StudentSharing.find params[:id]
+
+    # To prevent hacking role
+    existed_user = User.unblocked.find_by_email params[:student_sharing][:email]
+    if existed_user 
+      params[:student_sharing][:role_id] = existed_user.role.id
+    end 
+
     if @user.update_attributes params[:student_sharing]
       session[:tab] = "user"
     end 
