3c147c7848152bde6561cb7bc8d222f25de5806a
Man Vuong <man.vuong@techpropulsionlabs.com>
2012-12-27 19:58:14 +0700

    Bump to version 0.5

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
-VERSION = '0.4'
+VERSION = '0.5'
 build_num = ENV['BUILD_NUMBER'] || Time.now.utc.strftime('%Y-%m-%d %H:%M:%S %Z')
 if match = build_num.match(/v+(\d+)$/) # In case the build number is come from Heroku, e.g, "v20", "v21", ...
   build_num = match[1]
