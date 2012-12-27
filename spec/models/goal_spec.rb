4b6def3b4404c286242827e25d0ae8ebd0838e32
Man Vuong <man.vuong@techpropulsionlabs.com>
2012-12-27 16:51:59 +0700

    Update model annotation

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -2,23 +2,19 @@
 #
 # Table name: goals
 #
-#  id                       :integer          not null, primary key
-#  student_id               :integer          not null
-#  curriculum_id            :integer          not null
-#  accuracy                 :float            default(0.0), not null
-#  baseline                 :float            default(0.0), not null
-#  baseline_date            :date             not null
-#  due_date                 :date             not null
-#  trial_days_total         :integer          not null
-#  trial_days_actual        :integer          not null
-#  grades_data_file_name    :string(255)
-#  grades_data_content_type :string(255)
-#  grades_data_file_size    :integer
-#  grades_data_updated_at   :datetime
-#  description              :text
-#  is_completed             :boolean          default(FALSE)
-#  created_at               :datetime         not null
-#  updated_at               :datetime         not null
+#  id                :integer          not null, primary key
+#  student_id        :integer          not null
+#  curriculum_id     :integer          not null
+#  accuracy          :float            default(0.0), not null
+#  baseline          :float            default(0.0), not null
+#  baseline_date     :date             not null
+#  due_date          :date             not null
+#  trial_days_total  :integer          not null
+#  trial_days_actual :integer          not null
+#  description       :text
+#  is_completed      :boolean          default(FALSE)
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
 #
 
 require 'spec_helper'
