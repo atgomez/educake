9b385a0aa6045d496d5a36c2996ef7506442a2fd
thuy.nguyen <thuy.nguyen@techpropulsionlabs.com>
2013-01-04 18:09:27 +0700

    rspec for schools controller

--- spec/controllers/super_admin/schools_controller_spec.rb
+++ spec/controllers/super_admin/schools_controller_spec.rb
@@ -2,23 +2,34 @@ require 'spec_helper'
 describe SuperAdmin::SchoolsController do
   render_views
   let(:super_admin) {FactoryGirl.create(:super_admin)}
-  # describe "Get index" do 
-  # 	it "successfully" do 
-  # 		get :index
-  # 		response.should be_success
-  #   end
-  # end
+  let(:school) {FactoryGirl.create(:school)}
+  before(:each) do
+    sign_in super_admin
+  end
+  describe "Get index" do 
+  	it "successfully" do 
+  		get :index
+    end
+  end
 
-  # describe "Get new" do 
-  # 	it "successfully" do 
-  # 		get :new 
-  # 		response.should be_success
-  # 	end
-  # end
+  describe "Get new" do 
+  	it "successfully" do 
+  		get :new 
+  		response.should be_success
+  	end
+  end
 
-  # describe "Get edit" do 
-  # 	it "successfully" do 
-  # 		get :edit 
-  # 	end
-  # end 
+  describe "Get edit" do 
+  	it "successfully" do 
+  		get :edit, :id => school.id 
+  		response.should be_success
+  	end
+  end 
+
+  describe "Delete destroy" do 
+  	it "successfully" do 
+  		delete :destroy, :id => school.id 
+  		response.should redirect_to super_admin_schools_path
+  	end 
+  end 
 end
