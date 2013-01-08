34a4a22639992956b4ba00da95b293e7a57c533e
thuy.nguyen <thuy.nguyen@techpropulsionlabs.com>
2013-01-08 18:01:56 +0700

    rspec for users controller

--- spec/controllers/super_admin/users_controller_spec.rb
+++ spec/controllers/super_admin/users_controller_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 describe SuperAdmin::UsersController do 
 	render_views
   let(:super_admin) {FactoryGirl.create(:super_admin)}
-  let(:school) {FactoryGirl.create(:school)}
+  let(:school) {FactoryGirl.create(:school_with_admin)}
   let(:teacher) {FactoryGirl.create(:teacher, :school => school)}
-	let(:admin) {FactoryGirl.create(:admin, :school => school)}  
   before(:each) do
     sign_in super_admin
   end
@@ -16,13 +15,50 @@ describe SuperAdmin::UsersController do
   	end 
   end 
 
-  
+  describe "POST create" do 
+  	it "successfully" do 
+      attrs = {:school_id => school.id, :first_name => "Jonh", :last_name => "Smith", 
+      :role_id => "2", :email => "jonh@gmail.com", :phone => "123456789", :notes => "how are you?"}
+      post :create, :user => attrs, :school_id => school.id, :back => "schools"
+      response.should redirect_to(super_admin_school_path(User.last.school))
+  	end 
+
+    context "unsuccessfully" do 
+      it "when invalid input" do
+        attrs = {:school_id => school.id, :first_name => "", :last_name => "Smith", 
+        :role_id => "2", :email => "jonh@gmail.com", :phone => "123456789", :notes => "how are you?"}
+        post :create, :user => attrs, :school_id => school.id, :back => "schools"
+        response.should render_template("new")
+      end 
+      it "when unexisted school" do 
+        attrs = {:school_id => 0, :first_name => "", :last_name => "Smith", 
+        :role_id => "2", :email => "jonh@gmail.com", :phone => "123456789", :notes => "how are you?"}
+        post :create, :user => attrs, :school_id => school.id, :back => "schools"
+        response.should render_template("edit")
+      end 
+    end 
+  end 
 
   describe "GET edit" do 
   	it "successfully" do 
   		get :edit, :school_id => school.id, :id => teacher.id
   		response.should render_template("edit")
   	end 
+  end 
+
+  describe "PUT update" do 
+    it "successfully" do 
+      attrs = {:school_id => teacher.school.id, :first_name => "aaa", :last_name => "Smith", 
+        :role_id => "2", :email => "jonh@gmail.com", :phone => "123456789", :notes => "how are you?"}
+      put :update, :id => teacher.id, :user => attrs, :school_id => school.id, :back => "schools"
+      response.should redirect_to(super_admin_school_path(teacher.school))
+    end 
+    it "unsuccessfully" do 
+      attrs = {:school_id => teacher.school.id, :first_name => "", :last_name => "Smith", 
+        :role_id => "2", :email => "jonh@gmail.com", :phone => "123456789", :notes => "how are you?"}
+      put :update, :id => teacher.id, :user => attrs, :school_id => school.id, :back => "schools"
+      response.should render_template("edit")
+    end
   end 
 
   describe "PUT blocked account" do 
@@ -54,8 +90,8 @@ describe SuperAdmin::UsersController do
   			response.should redirect_to students_path(:user_id => teacher.id)
   		end
   		it "with admin role" do 
-  			get :view_as, :id => admin.id 
-  			response.should redirect_to admin_teachers_path(:admin_id => admin.id)
+  			get :view_as, :id => school.admin.id 
+  			response.should redirect_to admin_teachers_path(:admin_id => school.admin.id)
   		end 
   	end
   end 
