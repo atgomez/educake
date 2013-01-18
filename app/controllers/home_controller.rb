ae4ca901d94420c1b7d260e35a6c4fe078a44ed5
thuy.nguyen <thuy.nguyen@techpropulsionlabs.com>
2013-01-18 10:41:53 +0700

    Update for subscribers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
-  authorize_resource :student
-
+  #authorize_resource :student
+  skip_before_filter :authenticate_user!
+  
   def index
-  	redirect_to("/students")
+  	if user_signed_in?
+  		redirect_to("/students")
+  	else
+  		redirect_to new_subscriber_path
+  	end 
   end
 end
