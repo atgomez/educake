1e88cbf58a99b161710fd40008e1ef85a0be7c24
Man Vuong <man.vuong@techpropulsionlabs.com>
2013-01-21 12:00:46 +0700

    Cannot use Base class in Grape API, use mount instead

--- app/api/educake_api/base.rb
+++
@@ -1,9 +0,0 @@
-module EducakeAPI
-  class Base < Grape::API
-    prefix "api"
-    format :json
-    version "v1"
-    # Use Rabl is optional
-    # formatter :json, Grape::Formatter::Rabl
-  end
-end
