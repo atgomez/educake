179ad7a4a75eec359d5d1c6df19065ea7e050bfb
Man Vuong <man.vuong@techpropulsionlabs.com>
2013-02-01 17:33:33 +0700

    Handle exception in importing curriculum

--- app/controllers/super_admin/curriculums_controller.rb
+++ app/controllers/super_admin/curriculums_controller.rb
@@ -96,7 +96,7 @@ class SuperAdmin::CurriculumsController < SuperAdmin::BaseSuperAdminController
           flash[:alert] = generate_import_errors(result)
         end      
       end
-    rescue ArgumentError => exc
+    rescue *[ArgumentError, CSV::MalformedCSVError] => exc
       ::Util.log_error(exc, "SuperAdmin::CurriculumsController#import")
       flash[:alert] = I18n.t("curriculum.import_failed_with_file_format")
     rescue Exception => exc
